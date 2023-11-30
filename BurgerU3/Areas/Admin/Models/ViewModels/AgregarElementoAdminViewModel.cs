f462a1ff25a0c43715a13347f5866f9e1d54a495
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-11-30 06:59:58 -0600

    c

--- BurgerU3/Areas/Admin/Models/ViewModels/AgregarElementoAdminViewModel.cs
+++ BurgerU3/Areas/Admin/Models/ViewModels/AgregarElementoAdminViewModel.cs
@@ -4,10 +4,7 @@ namespace BurgerU3.Areas.Admin.Models.ViewModels
 {
     public class AgregarElementoAdminViewModel
     {
-        //public Menu menu { get; set; } = new();
-        //public IEnumerable<ClasificacionModel> ListaClasificaciones { get; set; } = null!;
-        //public IFormFile? Archivo { get; set; }
-
+        
         public Menu menu { get; set; } = new();
         public IEnumerable<Clasificacion> ListaClasificaciones { get; set; } = null!;
         public IFormFile? Archivo { get; set; }
